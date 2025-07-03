b53c4a1c69b3bf10a8ee5cdb37764497a502ce77
Alexandra <al3xandra92@hotmail.com>
2025-07-03 15:37:57 +0200

    ✨ Add: MobileHeader and Hero components to the main page layout

--- frontend-test/src/app/page.tsx
+++ frontend-test/src/app/page.tsx
@@ -1,6 +1,8 @@
 
 import BottomNav from "@/components/BottomNav";
 import DesktopHeader from "@/components/DesktopHeader";
+import Hero from "@/components/Hero";
+import MobileHeader from "@/components/MobileHeader";
 import PostListWrapper from "@/components/PostListWrapper";
 
 
@@ -8,6 +10,8 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-white text-black p-6">
       <DesktopHeader />
+      <MobileHeader />
+      <Hero />
       <PostListWrapper />
       <BottomNav />
     </main>
